69c0e763b9a80fb32b572eb82c3a79c4c7109534
GabrielFarias-dev <invokerkfjr@gmail.com>
2024-09-07 18:07:44 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [{
+    titulo: "Sd Mathias",
+    descricao:
+        "Sd Mathias, é responsável pela parte eletrica e faz parte da equipe da piscina responsável pela segurança dos associados.",
+    link: "https://www.instagram.com/mathiasrocha83/",
+    tags: "soldado, salva-vidas"
+
+},
+{
+    titulo: "Sd Santos",
+    descricao:
+        "Sd Santos faz parte da equipe de manutençao e é responsável pela serviço de guardião de piscina, garantindo a segurança dos associados.",
+    link: "https://www.instagram.com/r.junin_/",
+    tags: "salva-vidas,de amarelo, baixinho"
+
+},
+{
+    titulo: "Sd Valdek",
+    descricao:
+        "Sd Valdek faz parte da equipe de manuntenção e é responsável pelo serviço de portaria, controle e entrada de associados.",
+    link: "https://www.instagram.com/gabrielfariasds/",
+    tags: "farda polo azul, fica la na frente"
+
+}
+]
+
