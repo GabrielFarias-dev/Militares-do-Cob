77a8e3351900ca9406128cd11e541e6e68be3cc4
Gabriel <invokerkfjr@gmail.com>
2026-03-10 20:03:21 -0400

    first commit

---
+++ Documents/atividade/my-daily-habts/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './componets/Footer.jsx'
+function App(){
+return(
+<div>
+<h1>My Daily Habits</h1>
+<p>Gerencie seus hábitos diários de forma simples e visual.</p>
+<Footer />
+</div>
+)
+}
+export default App
---
+++ Documents/atividade/my-daily-habts/src/componets/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+return(
+<footer>
+<small>Módulo04–DesenvolvimentoFront-endcomReact–ITEAM</small>
+</footer>
+)
+
+}
+export default Footer
